14a04f16a5d1d2a5de8a0af040d5a91423786a69
diogox <barretodiogo128@gmail.com>
2025-07-18 07:46:45 -0300

    nueva clase personalizada

--- pruebas/erroresPersonalizados.js
+++ pruebas/erroresPersonalizados.js
@@ -111,6 +111,40 @@ try {
 
 // Crear un error si el nombre de usuario tiene menos de 4 caracteres.
 
+class NombreCorto extends Error {
+
+    constructor(usuario) {
+        super (`este nombre es muy corto`)
+        this.name = 'NombreCorto'
+        this.usuario = usuario
+    }
+
+    sugerencias() {
+        console.log('El nombre debe tener minimo cuatro caracteres')
+    }
+}
+
+function analizarUsuario(usuario) {
+    if (typeof usuario !== 'string') {
+        console.log('usuario invalido, solo se permiten letras!')
+    } else if (usuario.length < 4) {
+        throw new NombreCorto(usuario)
+
+    } 
+
+    console.log(`bienvenido ${usuario}`)
+}
+
+try {
+    analizarUsuario('dio')
+
+} catch(error) {
+    if (error instanceof NombreCorto ) {
+        console.log(error.message)
+        error.sugerencias()
+    }
+}
+
 // Lanzar error si el texto NO termina en .jpg, .png o .gif.
 
 // Lanzar error si la contraseña tiene menos de 8 caracteres o no contiene un número.
