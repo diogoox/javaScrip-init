44a330a00fd67c9b0fd42179a4e5b1a15ff7a83f
diogox <barretodiogo128@gmail.com>
2025-08-12 11:57:45 -0300

    estructuras avanzadas

--- intermediate/02-advanced-structures.js
+++ intermediate/02-advanced-structures.js
@@ -70,8 +70,39 @@ console.log(sorded.includes(20))
 
 // find 
 
-sorded = [3, 8, 11, 12]
 let firtsEven = sorded.find(element => element % 2 === 0)// muestra solo el primer par
 console.log(firtsEven)
 
-//
+// findIndex
+
+sorded = [2, 3, 4]
+let firtsEvenIndex = sorded.findIndex(element => element % 2 === 0) // devuelve el primer indice que cumpla con la condicion
+console.log(firtsEvenIndex)
+
+// sets avanzados
+
+// operaciones
+
+// eliminacion de duplicados
+
+let numbersArray = [1, 2, 2, 3, 4, 5, 6, 6]
+numbersArray =[...new Set(numbersArray)]
+console.log(numbersArray)
+
+// Union
+
+const setA = new Set([1, 2, 3])
+const setB = new Set([1, 2, 4, 5, 6 ])
+const union = new Set([...setA, ...setB])
+
+console.log(union)
+
+// interseccion 
+
+const intersection = new Set([...setA].filter(element => setB.has(element)))
+console.log(intersection)
+
+// diferencia
+
+const difference = new Set([...setA].filter(element => !setB.has(element)))
+console.log(intersection)
