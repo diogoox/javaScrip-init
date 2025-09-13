c28ac1316fba58e768c8115430fc624eb15130ab
diogox <barretodiogo128@gmail.com>
2025-09-13 10:18:57 -0300

    repaso regex

---
+++ intermediate/07-async.js
@@ -0,0 +1,121 @@
+// programacion asincrona
+
+// codigo sincrono
+
+console.log('inicio') 
+
+for (let i = 0; i < 1000000; i++) {}
+
+console.log('fin')
+
+// event loop (bucle de eventos)
+
+//   componenetes del Event loop
+// call stack (pila de ejecucion)
+// web APis (APis del navegador) o Node.js: setTimeout()...
+// Task  Queue (cola de tareas) y MicrotasKQueue
+
+
+// flujo de event loop:
+// 1 call stack
+// 2 operaciones asincronas -> web APis o Node.js
+// 3 operacion termina -> la coloca en Task Queue o MicroTask Queue
+// 4 si call Stack vacio -> Mueve tareas del MicroTask Queue o Task Queue al call Stack
+// 5 el proceso se repite
+
+// codigo asincrono 
+
+// callback
+
+console.log('inicio')
+
+setTimeout(() => {
+    console.log('esto se ejecuta despues de 2 segundos')
+}, 2000)
+
+console.log('fin')
+
+// problema: callback hell
+
+function step1(callback) {
+    setTimeout(() => {
+        console.log('paso 1 completado')
+        callback()
+    }, 1000)
+}
+
+function step2(callback) {
+    setTimeout(() => {
+        console.log('paso 2 completado')
+        callback()
+    }, 1000)
+}
+function step3(callback) {
+    setTimeout(() => {
+        console.log('paso 3 completado')
+        callback()
+    }, 1000)
+}
+
+step1(() => {
+    step2(() => {
+        step3(() => {
+            console.log('todos los pasos completados')
+        })
+    })
+})
+
+// promesas
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const ok = true
+        if (ok) {
+            resolve('operacion exitosa')
+        } else {
+            reject('se ha producido un error')
+        }
+    }, 3000)
+})
+
+promise
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+// Encadenamiento de promesas
+
+function step1Promise() {
+    return new Promise(resolve => {
+        setTimeout(() => { 
+            console.log('paso 1 con promise completado')
+            resolve()
+        }, 1000)
+    })
+}
+
+function step2Promise() {
+    return new Promise(resolve => {
+        setTimeout(() => { 
+            console.log('paso 2 con promise completado')
+            resolve()
+        }, 1000)
+    })   
+}
+
+function step3Promise() {
+    return new Promise(resolve => {
+        setTimeout(() => { 
+            console.log('paso 3 con promise completado')
+            resolve()
+        }, 1000)
+    })
+}
+
+step1Promise()
+.then(step2Promise)
+.then(step3Promise)
+.then(console.log('todos los pasos con promesas completados'))
---
+++ intermediate/REPASO/regex.js
@@ -0,0 +1,62 @@
+// Encontrar todas las vocales en un texto
+
+const String = 'hola mundo'
+
+const Vocales = String.match(/[aeiou]/g)
+
+console.log(Vocales)
+
+// Validar que una palabra solo tenga letras
+
+function validarPalabra(palabra) {
+    return /^[a-zA-Z]+$/.test(palabra)
+}
+console.log(validarPalabra('h0la'))
+
+// Validar un número entero positivo
+
+function numeroEntero(number) {
+    return /^\d+$/.test(number)
+}
+
+console.log(numeroEntero(-1))
+
+// encontrar espacios en el texto
+
+function encontrarEspacios(string) {
+    return string.match(/\s+/g)
+}
+
+console.log(encontrarEspacios('hola mundo'))
+
+// Validar que una cadena empiece con 'H'
+
+function H(cadena) {
+     return /^H/.test(cadena)
+}
+console.log(H('Hola'))
+
+// Validar que termine con un punto
+
+function punto(texto) {
+    return /\.$/.test(texto)
+}
+console.log(punto('hol'))
+
+// Validar que haya al menos un número en el texto
+
+function validarNumero(t) {
+    return /\d+/.test(t)
+}
+
+console.log(validarNumero('hol1'))
+
+// Validar un correo sencillo
+
+function validarCorreo(correo) {
+    return /^[\w.-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/.test(correo)
+}
+
+console.log(validarCorreo())
+
+//
